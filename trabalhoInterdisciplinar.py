4b4f1f9b03181f82aaa22ade954d9f249544c247
Maria Fernanda Vargas <mariiafvargass@gmail.com>
2026-05-13 16:30:26 -0300

    Ligação Python -> Mysql

---
+++ trabalhoInterdisciplinar.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from mysql.connector import Error
+
+def criar_conexao():
+    try:
+        conexao = mysql.connector.connect(
+            host='127.0.0.1',
+            user='root',
+            password='Senac2026',
+            database='escola'
+        )
+        return conexao
+ 
+    except Error as e:
+        print(f"Erro ao conectar ao MySQL: {e}")
+        return None
+    
