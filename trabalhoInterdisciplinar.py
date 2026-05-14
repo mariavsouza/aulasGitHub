915188a789e067134da1322bdcd1af9a8e38f6aa
mariavsouza <mariiafvargass@gmail.com>
2026-05-14 14:32:07 -0300

    Esboço da criação de database e table do mysql

--- trabalhoInterdisciplinar.py
+++ trabalhoInterdisciplinar.py
@@ -7,10 +7,41 @@ def criar_conexao():
             host='127.0.0.1',
             user='root',
             password='Senac2026',
-            database='escola'
+            database='sistemaEscolar'
         )
         return conexao
  
     except Error as e:
         print(f"Erro ao conectar ao MySQL: {e}")
         return None
+    
+def criar_database():
+    conexao = criar_conexao()
+ 
+    if conexao:
+        cursor = conexao.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS sistemaEscolar")
+        print("Banco de dados criado com sucesso! :) ")
+
+    cursor.close()
+    conexao.close()
+
+def criar_table():
+    conexao = criar_conexao()
+ 
+    if conexao:
+        cursor = conexao.cursor()
+        criar_table = """
+        CREATE TABLE IF NOT EXISTS alunos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL,
+        data_nascimento DATETIME,
+        turma VARCHAR(50),
+        notas VARCHAR(255),
+        media DECIMAL(4, 2),
+        situacao VARCHAR(50)"""
+
+    cursor.execute(criar_table)
+    cursor.close()
+    conexao.close()
+    
