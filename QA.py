853ddb2a721093b7c7b77cd4866656e5bda12362
mariavsouza <mariiafvargass@gmail.com>
2026-05-18 14:30:27 -0300

    Update QA.py

--- QA.py
+++ QA.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 notas = []
+alunos = []
+
 def validar_nome(nome):
     if not nome.strip():
         print("O nome não pode estar vazio.")
@@ -9,7 +11,7 @@ def validar_nome(nome):
         print("O nome deve conter apenas letras.")
         return False
  
-    return True, "Nome válido."
+    return True
  
  
 def validar_nascimento(data_nascimento):
@@ -17,32 +19,38 @@ def validar_nascimento(data_nascimento):
         data = datetime.strptime(data_nascimento, "%d/%m/%Y")
  
         if data > datetime.now():
-            return False, "A data de nascimento não pode ser futura."
+            print("A data de nascimento não pode ser futura.")
+            return False
  
-        return True, "Data de nascimento válida."
+        return True,
  
     except ValueError:
-        return False, "Formato inválido. Use DD/MM/AAAA."
+        print("Formato inválido. Use DD/MM/AAAA.")
+        return False
  
  
 def validar_turma(turma):
     if not turma.strip():
-        return False, "A turma não pode estar vazia."
+        print("A turma não pode estar vazia.")
+        return False
  
-    return True, "Turma válida."
+    return True
  
 def validar_notas(notas):
     if not isinstance(notas, list):
-        return False, "As notas devem ser uma lista."
+        print("As notas devem ser uma lista.")
+        return False
  
     for nota in notas:
         if not isinstance(nota, (int, float)):
-            return False, "Todas as notas devem ser numéricas."
+            print("Todas as notas devem ser numéricas.")
+            return False
  
         if nota < 0 or nota > 10:
-            return False, "As notas devem estar entre 0 e 10."
+            print("As notas devem estar entre 0 e 10.")
+            return False
  
-    return True, "Notas válidas."
+    return True
  
  
 def calcular_media(notas):
@@ -68,21 +76,25 @@ def definir_situacao(media):
         return "Reprovado"
 
 def cadastrar_aluno():
+    while True:
+        nome = input("Nome do aluno: ")
+        if not validar_nome(nome):
+            return False
+        
+        data_nascimento = input("Data de Nascimento: ")
+        if not validar_nascimento(data_nascimento):
+            return False
 
-    cadastro = []
+        turma = input("Turma: ")
+        if not validar_turma(turma):
+            return False
 
-    nome = input("Nome do aluno: ")
-    if not validar_nome():
-        return False
-    data_nascimento = datetime(input("Data de Nascimento: "))
-    validar_nascimento()
-    turma = input("Turma: ")
-    validar_turma()
+        dados = [nome, data_nascimento, turma]
+        alunos.append(dados)
 
-    dados = [nome, data_nascimento, turma]
-    cadastro.append(dados)
 
-    print("Aluno cadastrado com sucesso!")
+        print("Aluno cadastrado com sucesso!")
+        break
 
 
 def listar_aluno():
@@ -95,21 +107,52 @@ def editar_dados():
     ...
 
 def adicionar_notas():
+    id_aluno = int(input("Digite o ID do aluno: "))
+
+    if id_aluno not in alunos:
+        print("Aluno não encontrado.")
+        return
+
     entrada = float(input("Digite a nota: "))
-    notas.append(entrada)
+    alunos[id_aluno]["notas"].append(entrada)
+
     print("Nota adicionada!")
 
 def remover_notas():
-    ...
+    id_aluno = int(input("Digite o ID do aluno: "))
+
+    if id_aluno not in alunos:
+        print("Aluno não encontrado :( ")
+        return
+
+    if not alunos[id_aluno]["notas"]:
+        print("Esse aluno não possui notas :(")
+        return
+
+    print("Notas:", alunos[id_aluno]["notas"])
+    nota = float(input("Digite a nota que deseja remover: "))
+
+    if nota in alunos[id_aluno]["notas"]:
+        alunos[id_aluno]["notas"].remove(nota)
+        print("Nota removida!")
+    else:
+        print("Nota não encontrada.")
 
 def caucular_media():
-    
+    id_aluno = int(input("Digite o ID do aluno: "))
+
+    if id_aluno not in alunos:
+        print("Aluno não encontrado :(")
+        return
+
+    notas = alunos[id_aluno]["notas"]
+
     if len(notas) > 0:
         media = sum(notas) / len(notas)
         print(f"\nVocê digitou {len(notas)} notas.")
         print(f"A média final é: {media:.2f}")
     else:
-        print("Nenhuma nota foi registrada.")
+        print("Nenhuma nota foi registrada :(")
 
 def situção_aluno():
     ...
