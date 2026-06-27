00f795750e95ec2b35323b7df1ec3d3904c0f284
Maria Fernanda Vargas <mariiafvargass@gmail.com>
2026-06-27 13:27:53 -0300

    atualizando e corrigindo alguns erros

--- mafe.py
+++ mafe.py
@@ -4,46 +4,36 @@ from mysql.connector import Error
 from datetime import datetime
 from colorama import Fore, init
 init()
-#listas pré estabelecidas | colocar as materias em determinadas turmas
+
+#id's
+id_aluno = 1
+id_professor = 1
+
+#listas pré estabelecidas 
 alunos = []
 professores = []
+notas = []
 materias = {
-    "1EM": [
-        "português", "inglês", "artes", "educação física",
-        "matemática", "geografia", "história", "sociologia",
-        "filosofia", "biologia", "química", "física",
-        "projeto de vida"
-    ],
-    "2EM": [
-        "português", "inglês", "artes", "educação física",
-        "matemática", "geografia", "história", "sociologia",
-        "filosofia", "biologia", "química", "física",
-        "projeto de vida"
-    ],
-    "3EM": [
-        "português", "inglês", "artes", "educação física",
-        "matemática", "geografia", "história", "sociologia",
-        "filosofia", "biologia", "química", "física",
-        "projeto de vida"
-    ]
+    "1EM": ["A", "B", "R"],
+    "2EM": ["A", "B", "R"],
+    "3EM": ["A", "B", "R"]
 }
-
 
 #conexão com mySQL (sem conexão nas defs ainda)
 def criar_conexao():
     try:
         conexao = mysql.connector.connect(
-            host='127.0.0.1',
-            user='root',
+           host='127.0.0.1',
+           user='root',
             password='Senac2026',
-            database='sistemaEscolar'
+           database='sistemaEscolar'
         )
         return conexao
- 
+
     except Error as e:
-        print(f"Erro ao conectar ao MySQL: {e}")
-        return None
-    
+       print(f"Erro ao conectar ao MySQL: {e}")
+       return None
+
 #validações
 def validar_nome(nome):
 
@@ -57,44 +47,21 @@ def validar_nome(nome):
 
     return True
 
+
 def validar_nascimento(data_nascimento):
     try:
         data = datetime.strptime(data_nascimento, "%d/%m/%Y")
- 
+
         if data > datetime.now():
             print("A data de nascimento não pode ser futura.")
             return False
- 
+
         return True
- 
+
     except ValueError:
         print("Formato inválido. Use DD/MM/AAAA.")
         return False
- 
 
-def validar_turma(turma):
-    if not turma.strip():
-        print("A turma não pode estar vazia.")
-        return False
- 
-    return True
-
-
-def validar_notas(notas):
-    if not isinstance(notas, list):
-        print("As notas devem ser uma lista.")
-        return False
- 
-    for nota in notas:
-        if not isinstance(nota, (int, float)):
-            print("Todas as notas devem ser numéricas.")
-            return False
- 
-        if nota < 0 or nota > 10:
-            print("As notas devem estar entre 0 e 10.")
-            return False
- 
-    return True
 
 def validar_materia(turma, materia):
 
@@ -102,43 +69,35 @@ def validar_materia(turma, materia):
         print("Turma não encontrada.")
         return False
 
-    if materia.lower() not in materias[turma]:
+    if materia not in materias[turma]:
         print("Matéria inválida.")
         return False
 
     return True
 
-#defs funções
-def mostrar_materias(turma):
 
-    if turma not in materias:
-        print("Turma não encontrada")
-        return
-
-    print(f"\nMatérias da turma {turma}:\n")
-
-    for materia in materias[turma]:
-        print("-", materia)
-
+#cadastros
 def cadastrar_aluno():
+    global id_aluno
 
-    nome = input("Nome do aluno: ")
+    nome = input("Nome do aluno: ").strip()
 
     if not validar_nome(nome):
         return
 
-    data_nascimento = input("Data de Nascimento: ")
+    data_nascimento = input("Data de Nascimento: ").strip()
 
     if not validar_nascimento(data_nascimento):
         return
 
-    turma = input("Turma (1EM, 2EM ou 3EM): ")
+    turma = input("Turma (1EM, 2EM ou 3EM): ").upper().strip()
 
     if turma not in materias:
         print("Turma inválida.")
         return
 
     aluno = {
+        "id": id_aluno,
         "nome": nome,
         "nascimento": data_nascimento,
         "turma": turma,
@@ -146,42 +105,46 @@ def cadastrar_aluno():
     }
 
     alunos.append(aluno)
+    id_aluno += 1
 
-    print("Aluno cadastrado com sucesso!")
+    print(f"Aluno cadastrado com sucesso! ID: {aluno['id']}")
+
 
 def cadastrar_prof():
+    global id_professor
 
-    nome = input("Nome do Professor: ")
+    nome = input("Nome do Professor: ").strip()
 
     if not validar_nome(nome):
         return
 
-    turma = input("Turma (1EM, 2EM ou 3EM): ")
+    turma = input("Turma (1EM, 2EM ou 3EM): ").upper().strip()
 
     if turma not in materias:
         print("Turma inválida.")
         return
 
     print("\nMatérias disponíveis:")
+    print("\nA | Algorítimos;\nB | Banco de Dados\nR | Requisitos")
 
-    for materia in materias[turma]:
-        print("-", materia)
-
-    materia = input("\nEscolha a matéria: ").lower()
+    materia = input("\nQue matéria você leciona?: ").upper().strip()
 
     if materia not in materias[turma]:
         print("Matéria inválida.")
         return
 
     professor = {
+        "id": id_professor,
         "nome": nome,
         "turma": turma,
         "materia": materia
     }
 
     professores.append(professor)
+    id_professor += 1
 
-    print("Professor cadastrado com sucesso!")
+    print(f"Professor cadastrado com sucesso! ID: {professor['id']}")
+
 
 def cadastrar_turma():
 
@@ -191,24 +154,12 @@ def cadastrar_turma():
         print("Turma já cadastrada.")
         return
 
-    materias[nome_turma] = [
-        "português",
-        "inglês",
-        "artes",
-        "educação física",
-        "matemática",
-        "geografia",
-        "história",
-        "sociologia",
-        "filosofia",
-        "biologia",
-        "química",
-        "física",
-        "projeto de vida"
-    ]
+    materias[nome_turma] = ["A", "B", "R"]
 
     print("Turma cadastrada com sucesso!")
 
+
+#notas
 def adicionar_nota():
 
     nome = input("Nome do aluno: ")
@@ -217,27 +168,31 @@ def adicionar_nota():
 
         if aluno["nome"].lower() == nome.lower():
 
-            materia = input("Matéria: ").lower()
+            materia = input("Matéria: ").upper()
 
             if not validar_materia(aluno["turma"], materia):
                 return
 
-        try:
-            nota = float(input("Nota: "))
-        except ValueError:
-            print("Nota inválida.")
-            return
-        
-        if nota < 0 or nota > 10:
+            try:
+                nota = float(input("Nota: "))
+            except ValueError:
                 print("Nota inválida.")
                 return
 
-        aluno["notas"][materia] = nota
+            if nota < 0 or nota > 10:
+                print("Nota inválida.")
+                return
 
-        print("Nota adicionada com sucesso!")
-    return
+            if materia not in aluno["notas"]:
+                aluno["notas"][materia] = []
+
+            aluno["notas"][materia].append(nota)
+
+            print("Nota adicionada com sucesso!")
+            return
 
     print("Aluno não encontrado.")
+
 
 def editar_nota():
 
@@ -247,13 +202,24 @@ def editar_nota():
 
         if aluno["nome"].lower() == nome.lower():
 
-            materia = input("Matéria: ").lower()
+            materia = input("Matéria: ").upper()
 
             if materia in aluno["notas"]:
 
-                nova_nota = float(input("Nova nota: "))
+                print("Notas:", aluno["notas"][materia])
 
-                aluno["notas"][materia] = nova_nota
+                try:
+                    indice = int(input("Qual nota deseja editar?: ")) - 1
+                    nova_nota = float(input("Nova nota: "))
+                except:
+                    print("Entrada inválida.")
+                    return
+
+                if indice < 0 or indice >= len(aluno["notas"][materia]):
+                    print("Índice inválido.")
+                    return
+
+                aluno["notas"][materia][indice] = nova_nota
 
                 print("Nota atualizada!")
 
@@ -264,61 +230,6 @@ def editar_nota():
 
     print("Aluno não encontrado.")
 
-def editar_alunos():
-
-    nome = input("Nome do aluno: ")
-
-    for aluno in alunos:
-
-        if aluno["nome"].lower() == nome.lower():
-
-            print("1 - Nome")
-            print("2 - Data de Nascimento")
-            print("3 - Turma")
-
-            opcao = input("Escolha: ")
-
-            if opcao == "1":
-                aluno["nome"] = input("Novo nome: ")
-
-            elif opcao == "2":
-                aluno["nascimento"] = input("Nova data: ")
-
-            elif opcao == "3":
-                aluno["turma"] = input("Nova turma: ")
-
-            print("Aluno atualizado!")
-            return
-
-    print("Aluno não encontrado.")
-
-def editar_prof():
-
-    nome = input("Nome do professor: ")
-
-    for professor in professores:
-
-        if professor["nome"].lower() == nome.lower():
-
-            print("1 - Nome")
-            print("2 - Turma")
-            print("3 - Matéria")
-
-            opcao = input("Escolha: ")
-
-            if opcao == "1":
-                professor["nome"] = input("Novo nome: ")
-
-            elif opcao == "2":
-                professor["turma"] = input("Nova turma: ")
-
-            elif opcao == "3":
-                professor["materia"] = input("Nova matéria: ")
-
-            print("Professor atualizado!")
-            return
-
-    print("Professor não encontrado.")
 
 def remover_nota():
 
@@ -328,11 +239,23 @@ def remover_nota():
 
         if aluno["nome"].lower() == nome.lower():
 
-            materia = input("Matéria: ").lower()
+            materia = input("Matéria: ").upper()
 
             if materia in aluno["notas"]:
 
-                del aluno["notas"][materia]
+                print("Notas:", aluno["notas"][materia])
+
+                try:
+                    indice = int(input("Qual nota deseja remover?: ")) - 1
+                except:
+                    print("Entrada inválida.")
+                    return
+
+                if indice < 0 or indice >= len(aluno["notas"][materia]):
+                    print("Índice inválido.")
+                    return
+
+                aluno["notas"][materia].pop(indice)
 
                 print("Nota removida!")
 
@@ -343,6 +266,8 @@ def remover_nota():
 
     print("Aluno não encontrado.")
 
+
+#remover
 def remover_aluno():
 
     nome = input("Nome do aluno: ")
@@ -357,6 +282,7 @@ def remover_aluno():
             return
 
     print("Aluno não encontrado.")
+
 
 def remover_prof():
 
@@ -373,6 +299,7 @@ def remover_prof():
 
     print("Professor não encontrado.")
 
+
 def remover_turma():
 
     nome_turma = input("Nome da turma: ")
@@ -386,64 +313,162 @@ def remover_turma():
     else:
         print("Turma não encontrada.")
 
-def parecer_aluno():
-
-    nome = input("Nome do aluno: ")
+#outros
+def editar_aluno():
+    nome = input("Nome do aluno que deseja editar: ").strip()
 
     for aluno in alunos:
-
         if aluno["nome"].lower() == nome.lower():
 
-            if len(aluno["notas"]) == 0:
-                print("Aluno sem notas.")
+            print("\nO que deseja editar?")
+            print("1 - Nome")
+            print("2 - Data de Nascimento")
+            print("3 - Turma")
+
+            opcao = input("Escolha: ").strip
+
+            if opcao == "1":
+                novo_nome = input("Novo nome: ").strip()
+                if validar_nome(novo_nome):
+                    aluno["nome"] = novo_nome
+                    print("Nome atualizado com sucesso!")
+            elif opcao == "2":
+                nova_data = input("Nova data de nascimento: ").strip()
+                if validar_nascimento(nova_data):
+                    aluno["nascimento"] = nova_data
+                    print("Data de nascimento atualizada!")
+            elif opcao == "3":
+                nova_turma = input("Nova turma: ").upper().strip()
+                if nova_turma in materias:
+                    aluno["turma"] = nova_turma
+                    print("Turma atualizada!")
+                else:
+                    print("Turma inválida.")
+            else:
+                print("Opção inválida.")
+            return
+        
+    print("Aluno não encontrado.")
+
+def editar_prof():
+    nome = input("Nome do professor que deseja editar: ").strip()
+
+    for professor in professores:
+        if professor["nome"].lower() == nome.lower():
+
+            print("\nO que deseja editar?")
+            print("1 - Nome")
+            print("2 - Turma")
+            print("3 - Matéria")
+
+            opcao = input("Escolha: ")
+
+            if opcao == "1":
+                novo_nome = input("Novo nome: ").strip()
+                if validar_nome(novo_nome):
+                    professor["nome"] = novo_nome
+                    print("Nome atualizado com sucesso!")
+            elif opcao == "2":
+                nova_turma = input("Nova turma: ").upper().strip()
+                if nova_turma in materias:
+                    professor["turma"] = nova_turma
+                    print("Turma atualizada!")
+                else:
+                    print("Turma inválida.")
+            elif opcao == "3":
+                print("\nMatérias disponíveis:")
+                print("A | Algorítimos")
+                print("B | Banco de Dados")
+                print("R | Requisitos")
+
+                nova_materia = input("Nova matéria: ").upper().strip()
+
+                if validar_materia(professor["turma"], nova_materia):
+                    professor["materia"] = nova_materia
+                    print("Matéria atualizada!")
+            else:
+                print("Opção inválida.")
+
+            return
+
+    print("Professor não encontrado.")
+
+#parecer do aluno
+def parecer_aluno():
+
+    nome = input("Nome do aluno: ").strip()
+
+    for aluno in alunos:
+        if aluno["nome"].lower() == nome.lower():
+
+            print(f"\nID: {aluno['id']}")
+            print(f"Nome do Aluno: {aluno['nome']}")
+            print(f"Data de Nascimento: {aluno['nascimento']}")
+            print(f"Turma do Aluno: {aluno['turma']}")
+
+            if not aluno["notas"]:
+                print("Sem notas cadastradas.")
                 return
 
-            media = sum(aluno["notas"].values()) / len(aluno["notas"])
+            for materia, notas in aluno["notas"].items():
 
-            print(f"Média: {media:.1f}")
+                print(f"\nMatéria: {materia}")
 
-            if media >= 7:
-                print("APROVADO")
+                for i, nota in enumerate(notas, start=1):
+                    print(f"Nota {i}: {nota}")
 
-            elif media >= 5:
-                print("RECUPERAÇÃO")
+                media = sum(notas) / len(notas)
+                print(f"Média: {media:.1f}")
 
-            else:
-                print("REPROVADO")
+                if media >= 7:
+                    situacao = f"{Fore.GREEN}Aprovado{Fore.RESET}"
+                elif media >= 5:
+                    situacao = f"{Fore.YELLOW}Em Recuperação{Fore.RESET}"
+                else:
+                    situacao = f"{Fore.RED}Reprovado{Fore.RESET}"
+
+                print(f"Situação: {situacao}")
 
             return
 
     print("Aluno não encontrado.")
 
-#menus #COMPLETOS
+#menus | COMPLETOS
 def menu_aluno():
     while True:
         print("\n --- MENU DE AÇÕES DO ALUNO\\RESPONSÁVEL ---")
         print("1 - Visualizar parecer do aluno")
         print("2 - Trocar Forma de Login")
         print("0 - Sair do Sistema")
-        actAluno = input("\nO que você deseja fazer: ")
+
+        actAluno = input("\nO que você deseja fazer: ").strip()
 
         if actAluno == "1":
             parecer_aluno()
         elif actAluno == "2":
             menu_inicial()
         elif actAluno == "0":
-            print("\nVocê saiu do Sistema Instituto D'Souza. Encerando serviços...")
-            break
+           print("\nVocê saiu do Sistema Instituto D'Souza. Encerando serviços...")
+           break
         else:
             print(">>ERRO<<\nEscolha uma opção válida mostrada acima!")
             continue
-    
-def menu_professor():
-    while True:
-        professor = input("Digite seu nome:")
 
-        if professor in professores:
-            continue
-        else: 
+def menu_professor():
+    
+    nome = input("Digite seu nome: ").strip()
+    while True:
+
+        professor_encontrado = None
+
+        for prof in professores:
+            if prof["nome"].lower() == nome.lower():
+                professor_encontrado = prof
+                break
+
+        if not professor_encontrado:
             print("Usuário não encontrado!")
-            exit
+            return
 
         print("\n --- MENU DE AÇÕES DO PROFESSOR ---")
         print("1 - Adicionar Notas")
@@ -451,6 +476,7 @@ def menu_professor():
         print("3 - Editar Notas")
         print("4 - Trocar Forma de Login")
         print("0 - Sair do Sistema ")
+
         actProf = input("\nO que você deseja fazer: ")
 
         if actProf == "1":
@@ -462,25 +488,26 @@ def menu_professor():
         elif actProf == "4":
             menu_inicial()
         elif actProf == "0":
-            print("\nVocê saiu do Sistema Instituto D'Souza. Encerando serviços...")
-            break
+           print("\nVocê saiu do Sistema Instituto D'Souza. Encerando serviços...")
+           break
         else:
             print(">>ERRO<<\nEscolha uma opção válida mostrada acima!")
             continue
-        
+
 def menu_secretario():
-     while True:
+    while True:
         print("\n --- MENU DE AÇÕES DO SECRETÁRIO---")
         print("1 - Cadastrar Aluno")
         print("2 - Remover Aluno")
         print("3 - Cadastrar Professor")
         print("4 - Remover Professor")
-        print("5  - Cadastrar Turma")
+        print("5 - Cadastrar Turma")
         print("6 - Remover Turma")
         print("7 - Editar Dados dos Aluno")
         print("8 - Editar Dados do Professor")
         print("9 - Trocar Forma de Login")
         print("0 - Sair do Sistema ")
+
         actSecre = input("\nO que você deseja fazer: ")
 
         if actSecre == "1":
@@ -496,40 +523,41 @@ def menu_secretario():
         elif actSecre == "6":
             remover_turma()
         elif actSecre == "7":
-            editar_alunos()
+            editar_aluno()
         elif actSecre == "8":
             editar_prof()
         elif actSecre == "9":
             menu_inicial()
         elif actSecre == "0":
-            print("\nVocê saiu do Sistema Instituto D'Souza. Encerando serviços...")
-            break
+           print("\nVocê saiu do Sistema Instituto D'Souza. Encerando serviços...")
+           break
         else:
             print(">>ERRO<<\nEscolha uma opção válida mostrada acima!")
             continue
 
+
 def menu_inicial():
-    while True:  
+    while True:
         print("\n --- OPÇÕES DE LOGIN ---")
         print("1 - Aluno\\Responsável")
         print("2 - Professor(a)")
         print("3 - Secretário")
         print("0 - Sair do Sistema")
-        
+
         login = input("\nQual a sua forma de login?: ")
 
         if login == "1":
-            return menu_aluno()
+            menu_aluno()
         elif login == "2":
-            return menu_professor()
+            menu_professor()
         elif login == "3":
-            return menu_secretario()       
+            menu_secretario()
         elif login == "0":
-            print("\nVocê saiu do Sistema Instituto D'Souza. Encerando serviços...")
-            break
+           print("\nVocê saiu do Sistema Instituto D'Souza. Encerando serviços...")
+           break
         else:
             print(">>ERRO<<\nEscolha uma opção válida mostrada acima!")
-            return
+            continue
 
 print("\n----- Bem vindo ao sistema do Instituto D'Souza -----")
 menu_inicial()
